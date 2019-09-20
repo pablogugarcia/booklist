282e53916b6a2197f67c6c3268e4e5509a6cf1fa
Adam.Rackis <adam.rackis@riotgames.com>
2019-09-19 20:48:22 -0500

    workbox un-minify

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -107,6 +107,7 @@ module.exports = {
         getCache({ pattern: /^https:\/\/s3.amazonaws.com\/my-library-cover-uploads/, name: "local-images1" }),
         getCache({ pattern: /^https:\/\/my-library-cover-uploads.s3.amazonaws.com/, name: "local-images2" })
       ],
+      mode: "development"
       //importScripts: ["react/sw-manual/sw-index-bundle.js"]
     })
     //new BundleAnalyzerPlugin({ analyzerMode: "static" }),
